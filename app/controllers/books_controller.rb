503a5b56b1f897a0f99536d2ba94c3d8894e3d3a
Kalman Hazins <kalmanh@gmail.com>
2018-07-20 01:32:13 -0400

    Put more logic around extracting ISBN

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,5 +5,15 @@ class BooksController < ApplicationController
 
     # Substitute your amazon tag here
     @amazon_tag = 'jhu-ruby-coursera-20'
+
+    map_isbn(@books)
   end
+
+  private
+    def map_isbn(books)
+        books.each do |book|
+          isbn10_item = book["volumeInfo"]["industryIdentifiers"].find { |it| it['type'] == 'ISBN_10' } if book["volumeInfo"]["industryIdentifiers"]
+          book['isbn10'] = isbn10_item['identifier'] if isbn10_item
+        end
+    end
 end
