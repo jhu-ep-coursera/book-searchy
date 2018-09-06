79f6dae8c12222850a20c8eb213a1ccc04871ee5
Kalman Hazins <kalmanh@gmail.com>
2018-09-06 00:56:10 -0400

    Add search functionality and refine amazon display by isbn

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,11 +7,9 @@ class Book
   format :json
 
   def self.about term
-
     # replace 1 or more spaces in the term with a plus
     cleaned_up_term = term.gsub /\s+/, '+'
 
     get("", query: { q: "intitle:#{cleaned_up_term}"})["items"] || []
-
   end
 end
